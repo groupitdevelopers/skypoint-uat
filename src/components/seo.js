05a2ea7d60003866ed56ac56582fb3ce0bb0d8cb
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-02 15:20:57 +1000

    seo color

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,11 +5,8 @@ function Seo({ children }) {
   return (
     <>
       <meta name="google-site-verification" content="tVEOKsPlVKjH8tUdHrg1_2CR4MYIKednmYl6NWFBiCo" />
-      <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)"></meta>
-      <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)"></meta>
-      <meta name="msapplication-navbutton-color" content="#000000"></meta>
-      <meta name="apple-mobile-web-app-status-bar-style" content="#000000" media="(prefers-color-scheme: dark)"></meta>
-      <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff" media="(prefers-color-scheme: light)"></meta>
+      <meta name="theme-color" content="green" media="(prefers-color-scheme: dark)"></meta>
+      <meta name="theme-color" content="green" media="(prefers-color-scheme: light)"></meta>
       {children}
     </>
   )
