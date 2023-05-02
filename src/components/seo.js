429b068fa6a9bddbc0be063f0dae1190f82e8b0c
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-02 14:58:57 +1000

    seo meta color

--- src/components/seo.js
+++ src/components/seo.js
@@ -5,6 +5,7 @@ function Seo({ children }) {
   return (
     <>
       <meta name="google-site-verification" content="tVEOKsPlVKjH8tUdHrg1_2CR4MYIKednmYl6NWFBiCo" />
+      <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)"></meta>
       {children}
     </>
   )
