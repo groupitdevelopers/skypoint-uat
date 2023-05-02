87406f11c64d74612b046ba71d170054f7a697c9
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-02 15:12:50 +1000

    seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -4,12 +4,13 @@ import PropTypes from "prop-types"
 function Seo({ children }) {
   return (
     <>
+      <meta name="color-scheme" content="dark"></meta>
       <meta name="google-site-verification" content="tVEOKsPlVKjH8tUdHrg1_2CR4MYIKednmYl6NWFBiCo" />
       <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)"></meta>
       <meta name="theme-color" content="#ffffff" media="(prefers-color-scheme: light)"></meta>
       <meta name="msapplication-navbutton-color" content="#000000"></meta>
-      <meta name="apple-mobile-web-app-status-bar-style" content="#000000"></meta>
-      <meta name="color-scheme" content="dark"></meta>
+      <meta name="apple-mobile-web-app-status-bar-style" content="#000000" media="(prefers-color-scheme: dark)"></meta>
+      <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff" media="(prefers-color-scheme: light)"></meta>
       {children}
     </>
   )
