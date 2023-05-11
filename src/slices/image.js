5c969d1fbf9d88467308dd84dd41953f25313e50
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-12 09:40:56 +1000

    images alt update

--- src/slices/image.js
+++ src/slices/image.js
@@ -4,7 +4,7 @@ export const Image = props => {
   const anchorId = props.slice.primary.anchor_id
   const autoHeigh = props.slice.primary.auto_image_height
   const text = props.slice.primary.text
-  
+
   return (
     <section 
       className="container imageWrapper"
@@ -14,7 +14,7 @@ export const Image = props => {
         <div className="col p-0 txt-center">
           <img
             src={props.slice.primary.image.url}
-            alt={props.slice.primary.image.alt}
+            alt={props.slice.primary.image.alt ? props.slice.primary.image.alt : props.slice.primary.image.url}
             className={`${autoHeigh ? "autoHeight" : "fixHeight"}`}
           />
         </div>
