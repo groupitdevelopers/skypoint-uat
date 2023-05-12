ff2e4010d2a491e04c339dbe28a346e3b0fadc08
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-12 14:12:23 +1000

    gatsby pkg

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
   plugins: [
     "gatsby-plugin-sass",
     "gatsby-plugin-image",
+    "gatsby-plugin-gatsby-cloud",
     {
       resolve: "gatsby-source-filesystem",
       options: {
--- src/components/seo.js
+++ src/components/seo.js
@@ -5,9 +5,9 @@ const Seo = ({seoData, children }) => {
   return (
     <>
       <title>{seoData.tab_title +` | SkyPoint`}</title>
-      <script type="module" async defer src={link}></script>
+      <script async defer src={link}></script>
       <noscript><iframe title="frame" src="//www.googletagmanager.com/ns.html?id=GTM-WCSFZ5" height="0" width="0" style={{display:'none'}}></iframe></noscript>
-      <script type="module">{`
+      <script>{`
         (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
         j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
@@ -16,7 +16,7 @@ const Seo = ({seoData, children }) => {
         `}
       </script>
 
-      <script type="module">{`
+      <script>{`
         (function (i, s, o, g, r, a, m) {
           i['GoogleAnalyticsObject'] = r; i[r] = i[r] || function () {
             (i[r].q = i[r].q || []).push(arguments)
@@ -27,7 +27,7 @@ const Seo = ({seoData, children }) => {
         ga('send', 'pageview');
         `}
       </script>
-      <script type="module" src="https://al-dreamworld.secure-cdn.oc.accessoticketing.com/embed/accesso.js" data-accesso="l=en-au"></script>
+      <script type="text/javascript" src="https://al-dreamworld.secure-cdn.oc.accessoticketing.com/embed/accesso.js" data-accesso="l=en-au"></script>
       <meta name="description" content={seoData.meta_description} />
       <meta name="keywords" content={seoData.meta_keywords} />
       {seoData.og_title &&
@@ -44,9 +44,6 @@ const Seo = ({seoData, children }) => {
       }
 
       <meta name="google-site-verification" content="tVEOKsPlVKjH8tUdHrg1_2CR4MYIKednmYl6NWFBiCo" />
-      {/* <meta name="theme-color" content="#1a5d8d" media="(prefers-color-scheme: dark)"></meta>
-      <meta name="theme-color" content="#1a5d8d" media="(prefers-color-scheme: light)"></meta>
-      <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"></meta> */}
       <meta name="robots" content="all"/>
       <meta name="Googlebot" content="all"/>
       {children}
