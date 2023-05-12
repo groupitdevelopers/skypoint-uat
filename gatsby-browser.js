1b25ab3dac0f64ea8b0afba095990e6c681a6597
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-12 14:53:36 +1000

    browser

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,38 +1,12 @@
-// import * as React from "react";
-// import { PrismicPreviewProvider } from "gatsby-plugin-prismic-previews";
-// import { repositoryConfigs } from "./config/prismic/previews";
-// import "./src/skypoint.scss"
+import * as React from "react";
+import { PrismicPreviewProvider } from "gatsby-plugin-prismic-previews";
+import { repositoryConfigs } from "./config/prismic/previews";
+import "./src/skypoint.scss"
 
-// export const shouldUpdateScroll = () => false
-
-// export const wrapRootElement = ({ element }) => (
-//   <PrismicPreviewProvider repositoryConfigs={repositoryConfigs}>
-//     {element}
-//   </PrismicPreviewProvider>
-// );
-
-
-import * as React from 'react'
-import {
-  PrismicPreviewProvider,
-  componentResolverFromMap,
-} from 'gatsby-plugin-prismic-previews'
-
-import { linkResolver } from './config/prismic/link-resolver'
-import PageTemplate from './src/templates/page'
+export const shouldUpdateScroll = () => false
 
 export const wrapRootElement = ({ element }) => (
-  <PrismicPreviewProvider
-    repositoryConfigs={[
-      {
-        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
-        linkResolver,
-        componentResolver: componentResolverFromMap({
-          page: PageTemplate,
-        }),
-      },
-    ]}
-  >
+  <PrismicPreviewProvider repositoryConfigs={repositoryConfigs}>
     {element}
   </PrismicPreviewProvider>
-)
+);
--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,41 +1,16 @@
-// import * as React from "react";
-// import { PrismicPreviewProvider } from "gatsby-plugin-prismic-previews";
+import * as React from "react";
+import { PrismicPreviewProvider } from "gatsby-plugin-prismic-previews";
 
-// import { repositoryConfigs } from "./config/prismic/previews";
+import { repositoryConfigs } from "./config/prismic/previews";
 
-// import "./src/skypoint.scss"
-
-// export const wrapRootElement = ({ element }) => (
-//   <PrismicPreviewProvider repositoryConfigs={repositoryConfigs}>
-//     {element}
-//   </PrismicPreviewProvider>
-// );
-
-// export const onRenderBody = ({ setHtmlAttributes }) => {
-//   setHtmlAttributes({ lang: "en" });
-// };
-
-import * as React from 'react'
-import {
-  PrismicPreviewProvider,
-  componentResolverFromMap,
-} from 'gatsby-plugin-prismic-previews'
-
-import { linkResolver } from './config/prismic/link-resolver'
-import PageTemplate from './src/templates/page'
+import "./src/skypoint.scss"
 
 export const wrapRootElement = ({ element }) => (
-  <PrismicPreviewProvider
-    repositoryConfigs={[
-      {
-        repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
-        linkResolver,
-        componentResolver: componentResolverFromMap({
-          page: PageTemplate,
-        }),
-      },
-    ]}
-  >
+  <PrismicPreviewProvider repositoryConfigs={repositoryConfigs}>
     {element}
   </PrismicPreviewProvider>
-)
+);
+
+export const onRenderBody = ({ setHtmlAttributes }) => {
+  setHtmlAttributes({ lang: "en" });
+};
