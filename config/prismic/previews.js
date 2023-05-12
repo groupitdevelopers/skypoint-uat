76124c454f673558c98201da798510efd7e136b1
groupitdevelopers <groupitdevelopers@ardentleisure.com>
2023-05-12 14:38:44 +1000

    prev

--- config/prismic/previews.js
+++ config/prismic/previews.js
@@ -1,17 +1,8 @@
 import { linkResolver } from "./link-resolver";
 
 import pageTemplate from "../../src/templates/page";
-// import subPageTemplate from "../../src/templates/subPage";
+import subPageTemplate from "../../src/templates/subPage";
 
-/**
- * Prismic preview configuration for each repository in your app. This set of
- * configuration objects will be used with the `PrismicPreviewProvider`
- * higher order component.
- *
- * If your app needs to support multiple Prismic repositories, add each of
- * their own configuration objects here as additional elements.
- *
- */
 export const repositoryConfigs = [
   {
     repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
@@ -19,12 +10,12 @@ export const repositoryConfigs = [
     componentResolver: {
       homePage: pageTemplate
     },
-  // },
-  // {
-  //   repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
-  //   linkResolver,
-  //   componentResolver: {
-  //     homePage: subPageTemplate
-  //   },
+  },
+  {
+    repositoryName: process.env.GATSBY_PRISMIC_REPO_NAME,
+    linkResolver,
+    componentResolver: {
+      homePage: subPageTemplate
+    },
   }
 ]
