f94fc3a9ec1e1fc29c402df8e27afb32a6c53776
Rezso Dubiczki <rezso.dubiczki@dreamworld.com.au>
2024-11-27 12:28:23 +1000

    GTM Update

--- src/components/seo.js
+++ src/components/seo.js
@@ -6,15 +6,14 @@ const Seo = ({seoData, children }) => {
     <>
       <title>{seoData.tab_title +` | SkyPoint`}</title>
       <script async defer src={link}></script>
-      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WCSFZ5" height="0" width="0" style={{display:'none', visibility:'hidden'}}></iframe></noscript>
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJPN4FXJ"height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
       <script>{`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
                 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
                 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','GTM-WCSFZ5');</script>
-              `}
+                })(window,document,'script','dataLayer','GTM-PJPN4FXJ');`}
       </script>
-      <script type="text/javascript" src="https://al-dreamworld.secure-cdn.oc.accessoticketing.com/embed/accesso.js" data-accesso="l=en-au"></script>
+      <script type="text/javascript" src="https://al-skypoint.secure-cdn.oc.accessoticketing.com/embed/accesso.js" data-accesso="l=en-au"></script>
       <meta name="description" content={seoData.meta_description} />
       <meta name="keywords" content={seoData.meta_keywords} />
       {seoData.og_title &&
